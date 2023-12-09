20c7130831c17e7c1a0aa9c125028b707edc5fa0
Santiago Saldivar <ssaldivar1@fordham.edu>
2023-12-09 14:16:12 -0500

    new changes to supersift- adding new category

--- searx/settings_defaults.py
+++ searx/settings_defaults.py
@@ -22,6 +22,7 @@ SXNG_LOCALE_TAGS = ['all', 'auto'] + list(l[0] for l in sxng_locales)
 SIMPLE_STYLE = ('auto', 'light', 'dark')
 CATEGORIES_AS_TABS = {
     'general': {},
+    'government': {},
     'images': {},
     'videos': {},
     'news': {},
